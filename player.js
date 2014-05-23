0efdea7a64af7be4da426961647768f1dfe0ad67
Brian Uosseph <brian.uosseph@gmail.com>
2014-05-22 23:49:47 -0400

    Added filterType, but doesn't work yet

--- player.js
+++ player.js
@@ -1,20 +1,5 @@
 var playing = false;
 
-var filterCutoff = 20000;
-var filterCutoffInput = document.getElementById("filter-cutoff");
-filterCutoffInput.addEventListener("input", function() {
-	filterCutoff = filterCutoffInput.value;
-	console.log(filterCutoff);
-}, false);
-
-var volumeLevel = 1;
-var volumeInput = document.getElementById("volume");
-volumeInput.addEventListener("input", function() {
-	volumeLevel = volumeInput.value;
-	console.log(volumeLevel);
-}, false);
-
-
 var audioContext;
 if (typeof AudioContext !== "undefined") {
   audioContext = new AudioContext();
@@ -28,17 +13,36 @@ if (typeof AudioContext !== "undefined") {
 var mediaElement = document.getElementById('track1');	// Grab <audio>, which has already loaded the song for us!
 var source = audioContext.createMediaElementSource(mediaElement);	// Create an audio source from <audio>
 
-// In order to start playing the audio, you'll need to do so through the 
 
-// // // Creating a gain node
+// Gain
+var volumeLevel = 1;
+var volumeInput = document.getElementById("volume");
 var volume = audioContext.createGainNode();
-volume.gain.value = volumeLevel;
+volumeInput.addEventListener("input", function() {
+	volumeLevel = volumeInput.value;
+	volume.gain.value = volumeLevel;
+}, false);
 
-// // Creating a biquad filter node
-// // 5 fields: (type, frequency, detune, Q, gain)
+
+// Filter
+var filterCutoff = 20000;
+var filterCutoffInput = document.getElementById("filter-cutoff");
+var filterType = 0;
+var filterTypeInput = document.getElementById("filter-type");
 var filter = audioContext.createBiquadFilter();
-filter.type = 0; // 0 = lowpass, 1 = highpass, 2 = bandpass, 3 = lowshelf, 4 = highshelf, 5 = peaking, 6 = notch, 7 = allpass
+filter.type = filterType; // 0 = lowpass, 1 = highpass, 2 = bandpass, 3 = lowshelf, 4 = highshelf, 5 = peaking, 6 = notch, 7 = allpass
 filter.frequency.value = filterCutoff;
+// Doesn't change?
+filterTypeInput.addEventListener("input", function() {
+	filterType = filterTypeInput.value;
+	filter.type = filterType;
+}, false);
+filterCutoffInput.addEventListener("input", function() {
+	// Should convert from 0-100 to 0-20000
+	filterCutoff = filterCutoffInput.value;
+	filter.frequency.value = filterCutoff;
+}, false);
+
 
 
 
@@ -66,67 +70,3 @@ function pause() {
 	}
 }
 
-
-
-
-
-
-// var audioContext;
-// if (typeof AudioContext !== "undefined") {
-//   audioContext = new AudioContext();
-// } else if (typeof webkitAudioContext !== "undefined") {
-//   audioContext = new webkitAudioContext();  
-// } else {
-//   alert("Web Audio API is not supported in this browser");  // Letting the user know
-//   throw new Error("Web Audio API is not supported in this browser. :(");
-// }
-
-// // // If you don't want to use <audio> in your HTML...
-
-// // // URL of desired audio file
-// // var audioFileURL = "Warrior Concerto - The Glitch Mob.mp3";
-
-// // // Create XMLHttpRequest
-// // var request = new XMLHttpRequest();
-// // request.open("GET", audioFileURL, true);
-// // request.responseType = "arraybuffer"; // Required because file is binary, not text
-
-// // // Decode XMLHttpRequest Asychronously
-// // var audioData;
-// // request.onload = function() {
-// //   audioData = request.response;
-// // };
-// // request.send();
-
-// // ...
-
-
-
-
-
-
-
-// // Loading sounds
-// // AudioBuffer used for short/medium length sounds via XMLHttpRequest
-// var soundBuffer = null;
-// function loadSound(url) {
-//   // Create request for sound
-//   var request = new XMLHttpRequest();
-//   request.open('GET', url, true);
-//   request.responseType = 'arraybuffer'; // Required because file is binary, not text
-
-//   // Decode asycnhronously
-//   request.onload = function() {
-//     audioCtx.decodeAudioData(request.response, function(buffer) {
-//       soundBuffer = buffer;
-//     }, onError);
-//   }
-//   request.send();
-// }
-// loadSound("Warrior Concerto - The Glitch Mob.mp3")
-
-
-// var source = context.createBufferSource(); // creates a AudioBufferSourceNode
-// source.buffer = buffer;   // loads given AudioBuffer
-// source.connect(context.destination); // connects SourceNode to another node (DestNode). Note: AudioContext.destination returns a AudioDestinationNode
-// source.start(0);  // starts playing sound in SourceNode

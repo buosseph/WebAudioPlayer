1b7989b32ca20985495cf624188b07935c10cd75
Brian Uosseph <brian.uosseph@gmail.com>
2014-12-18 01:50:49 -0500

    Created model and collection for tracks, verified that tracks are being added into the playlist

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
+// Globals
+var playlist;
+
+
 if (window.File && window.FileReader && window.FileList && window.Blob) {
   // Great success! All the File APIs are supported.
 } else {
   alert('The File APIs are not fully supported by your current browser. Please upgrade to a newer version in order to use this application.');
 }
+
 
 function handleFileSelect(event) {
 	// Need access to original event if abstracting this function outside of AppView
@@ -12,12 +17,20 @@ function handleFileSelect(event) {
 	var files = event.originalEvent.dataTransfer.files;
 	var output = [];
 	for (var i = 0, f; f = files[i]; i++) {
-		output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-			f.size, ' bytes, last modified: ',
-			f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-			'</li>');
+		// output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+		// 	f.size, ' bytes, last modified: ',
+		// 	f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+		// 	'</li>');
+
+		var track = new Track({
+			title: 	f.name,
+			type: 	f.type,
+			size: 	f.size,
+		});
+
+		this.playlist.push(track);
 	}
-	$('#list').html('<ul>' + output.join(' ') + '</ul>');
+	// $('#list').html('<ul>' + output.join(' ') + '</ul>');
 }
 
 function handleDragOver(event) {
@@ -30,15 +43,35 @@ function handleDragOver(event) {
 (function($) {
 	// Models
 	Track = Backbone.Model.extend({
+		defaults: {
+			title: 		"",
+			type: 		"",
+			size: 		0,
+			buffer: 	[]
+		},
 		initialize: function() {
-			console.log("hi");
+			console.log(
+				this.get("title")
+				+ " " + this.get("type")
+				+ " " + this.get("size")
+			);
 		}
+	});
+
+	// Collections
+	Playlist = Backbone.Collection.extend({
+		model: Track
 	});
 
 	// Views
 	AppView = Backbone.View.extend({
 		el: '#app',
 		initialize: function() {
+			playlist = new Playlist();
+			playlist.on("add", function(track) {
+				console.log(playlist.length);
+			});
+
 			this.render();
 		},
 		render: function() {
@@ -53,8 +86,14 @@ function handleDragOver(event) {
 			handleDragOver(event);
 		},
 		handleFiles: function(event) {
+			$("#playlist").empty();
 			handleFileSelect(event);
-		}
+		},
+		addTrack: function(track) {
+			console.log("pushing track");
+			// var trackView = new TrackView({model: track});
+		},
+
 	});
 
 	var app = new AppView();
